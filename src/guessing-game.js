765b323a9a988b580cc8a3e2042b1b61224769cd
EugeneBykovsky <bykovsky.ssau@gmail.com>
2019-10-10 17:46:35 +0400

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,47 @@
 class GuessingGame {
-    constructor() {}
+  constructor(min, max, arr, point) {
+    this.min = min;
+    this.max = max;
+    this.arr = arr;
+    this.point = point;
+  }
 
-    setRange(min, max) {
-
+  setRange(min, max) {
+  let arr = [];
+    for (let i = min; i <= max; i++) {
+      arr.push(i);
     }
+    this.min = min;
+    this.max = max;
+    this.arr = arr;
+  }
 
-    guess() {
+  guess() {
+    this.point = this.arr[Math.floor(this.arr.length / 2)];
+    return this.point;
+  }
 
+  lower() {
+    let arr = [],
+        max = this.point;
+    for (let i = this.min; i <= max; i++) {
+        arr.push(i);
     }
+    this.min = arr[0];
+    this.max = arr[arr.length - 1];
+    this.arr = arr;
+  }
 
-    lower() {
-
+  greater() {
+    let arr = [],
+        min = this.point;
+    for (let i = min; i <= this.max; i++) {
+      arr.push(i);
     }
-
-    greater() {
-
-    }
+    this.min = arr[0];
+    this.max = arr[arr.length - 1];
+    this.arr = arr;
+  }
 }
 
 module.exports = GuessingGame;
